5e8eece1c8144f66e191e3a58637a6624c41f52e
harshchhabra <harshchhabra832000@gmail.com>
2024-08-05 18:47:42 +0530

    SC: Allow sum between numbers and also handle new lines between them

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 class StringCalculator {
   add(numbers){
     if (numbers === '') return 0;
+    let delimiter = /,|\n/;
+    const numArray = numbers.split(delimiter).map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -6,4 +6,21 @@ describe('StringCalculator', function() {
     const calculator = new StringCalculator();
     assert.strictEqual(calculator.add(''), 0);
   });
+
+  it('should return the number itself for a single number', function() {
+    const calculator = new StringCalculator();
+    assert.strictEqual(calculator.add('1'), 1);
+  });
+
+  it('should return the sum of two numbers', function() {
+    const calculator = new StringCalculator();
+    assert.strictEqual(calculator.add('1,2'), 3);
+  });
+
+  it('should return the sum of multiple numbers', function() {
+    const calculator = new StringCalculator();
+    assert.strictEqual(calculator.add('1,2,3'), 6);
+  });
+
+
 })
