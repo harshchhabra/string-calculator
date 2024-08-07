74ef4484aeb2328ca6c41c7e1dc7609e2bcde94a
harshchhabra <harshchhabra832000@gmail.com>
2024-08-07 16:48:52 +0530

    SC: refactor the code in the calculator function

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 class StringCalculator {
-  add(numbers){
-    if (numbers === '') return 0;
+  handleDelimeter() {
+    const parts = numbers.split("\n");
+    delimiter = parts[0].replace("//", "");
+    return parts[1];
+  }
+  add(numbers) {
+    if (numbers === "") return 0;
     let delimiter = /,|\n/;
-    if (numbers.startsWith('//')) {
-      const parts = numbers.split('\n');
-      delimiter = parts[0].replace("//", "")
-      numbers = parts[1];
+    if (numbers.startsWith("//")) {
+      numbers = this.handleDelimeter();
     }
     const numArray = numbers.split(delimiter).map(Number);
-    const negatives = numArray.filter(n => n < 0);
+    const negatives = numArray.filter((n) => n < 0);
     if (negatives.length > 0) {
-      throw new Error(`negative numbers are not allowed ${negatives.join(',')}`);
+      throw new Error(
+        `negative numbers are not allowed ${negatives.join(",")}`
+      );
     }
     return numArray.reduce((sum, num) => sum + num, 0);
   }
